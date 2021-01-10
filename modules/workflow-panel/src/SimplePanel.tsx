bb0b87ae0f9406b7292e56845d4dff0c8c824c80
zero88 <sontt246@gmail.com>
2021-01-10 22:23:46 +0700

    Try with react-flow-renderer

--- modules/workflow-panel/src/SimplePanel.tsx
+++ modules/workflow-panel/src/SimplePanel.tsx
@@ -1,49 +1,43 @@
-import React from 'react';
 import { PanelProps } from '@grafana/data';
-import { SimpleOptions } from 'types';
+import { stylesFactory } from '@grafana/ui';
 import { css, cx } from 'emotion';
-import { stylesFactory, useTheme } from '@grafana/ui';
+import React from 'react';
+import ReactFlow from 'react-flow-renderer';
+import { SimpleOptions } from 'types';
 
-interface Props extends PanelProps<SimpleOptions> {}
+interface Props extends PanelProps<SimpleOptions> {
+}
+
+const elements = [
+  {
+    id: '1',
+    type: 'input', // input node
+    data: { label: 'Input Node' },
+    position: { x: 250, y: 25 },
+  },
+  // default node
+  {
+    id: '2',
+    // you can also pass a React component as a label
+    data: { label: <div>Default Node</div> },
+    position: { x: 100, y: 125 },
+  },
+  {
+    id: '3',
+    type: 'output', // output node
+    data: { label: 'Output Node' },
+    position: { x: 250, y: 250 },
+  },
+  // animated edge
+  { id: 'e1-2', source: '1', target: '2', animated: true },
+  { id: 'e2-3', source: '2', target: '3' },
+];
 
 export const SimplePanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const theme = useTheme();
   const styles = getStyles();
   return (
-    <div
-      className={cx(
-        styles.wrapper,
-        css`
-          width: ${width}px;
-          height: ${height}px;
-        `
-      )}
-    >
-      <svg
-        className={styles.svg}
-        width={width}
-        height={height}
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        viewBox={`-${width / 2} -${height / 2} ${width} ${height}`}
-      >
-        <g>
-          <circle style={{ fill: `${theme.isLight ? theme.palette.greenBase : theme.palette.blue95}` }} r={100} />
-        </g>
-      </svg>
-
-      <div className={styles.textBox}>
-        {options.showSeriesCount && (
-          <div
-            className={css`
-              font-size: ${theme.typography.size[options.seriesCountSize]};
-            `}
-          >
-            Number of series: {data.series.length}
-          </div>
-        )}
-        <div>Text option value: {options.text}</div>
-      </div>
+    <div className={cx(styles.wrapper, css`width: ${width}px; height: ${height}px;`)}>
+      <ReactFlow elements={elements}/>
     </div>
   );
 };
